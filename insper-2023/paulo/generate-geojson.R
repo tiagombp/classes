0666a14d5a0076f52f4d7753a91e9070a53f5547
tiagombp <tiagombp@gmail.com>
2023-09-07 10:03:21 +0200

    updates

--- insper-2023/paulo/generate-geojson.R
+++ insper-2023/paulo/generate-geojson.R
@@ -2,12 +2,29 @@ library(tidyverse)
 library(sf)
 library(geojsonsf)
 library(rmapshaper)
+library(geobr)
 
 # dados daqui:
 # http://dados.prefeitura.sp.gov.br/pt_PT/dataset/distritos/resource/9e75c2f7-5729-4398-8a83-b4640f072b5d
 sp <- sf::read_sf('LAYER_DISTRITO')
 
-simple_sp <- rmapshaper::ms_simplify(sp)
+favelas <- read.csv("favelas-distritos.csv")
+
+nomes_distritos <- sp %>%
+  select(NOME_DIST) %>%
+  arrange(NOME_DIST) %>%
+  .$NOME_DIST
+
+favelas$NOME_DIST <- nomes_distritos
+
+favelas$pct <- round(100*favelas$ndf/favelas$ndp,2)
+favelas$cat <- cut(favelas$ndf/favelas$ndp, c(-Inf, 0, 0.1, 0.2, 0.3, 1), c("Sem favelas", "Até 10%", "Entre 10% e 20%", "Entre 20% e 30%", "Acima de 30%"))
+
+sp_data <- sp %>% left_join(favelas)
+
+simple_sp <- rmapshaper::ms_simplify(sp_data)
+
+
 
 sp_wgs84 <- sf::st_transform(simple_sp, 4326)
 #sf::st_crs(sp_wgs84)
@@ -16,4 +33,13 @@ ggplot(sp_wgs84) + geom_sf()
 
 sp_geo <- geojsonsf::sf_geojson(sp_wgs84, simplify = TRUE, digits = 6)
 
-write_file(sp_geo, 'dados2.json')
+write_file(sp_geo, 'dados_distritos.json')
+
+#### acrescentando a fronteira, para facilitar o fitExtent do D3
+
+mun_sp <- geobr::read_municipality(3550308)
+mun_sp_geo <- geojsonsf::sf_geojson(sp_wgs84, simplify = TRUE, digits = 6)
+
+#ggplot(sp_wgs84) + geom_sf() + geom_sf(data = simples_mun_sp, color = "blue", fill = "transparent")
+
+write_file(mun_sp_geo, 'dados_municipio.json')
--- insper-2023/paulo/script.js
+++ insper-2023/paulo/script.js
@@ -1,74 +1,162 @@
+const colors = {
+  "Sem favelas" : "#fff3e7",
+  "Até 10%" : "#FBCAB9", 
+  "Entre 10% e 20%" : "#FD9E7B", 
+  "Entre 20% e 30%" : "#FF723D", 
+  "Acima de 30%" : "orangered"
+}
+
+// funçãozinha para formatar os valores numéricos para um formato mais apresentável, com separador de milhar etc.
+function format(n) {
+  if (n == null) return 0;
+  return new Intl.NumberFormat("pt-BR", { style: 'decimal' }).format(n)
+}
+
+function make_color_legend() {
+
+  const cats = Object.keys(colors);
+
+  const leg = document.querySelector('.legenda');
+
+  cats.forEach(cat => {
+
+    const new_cat_container = document.createElement('div');
+    new_cat_container.classList.add('color-container');
+
+    const new_color_key = document.createElement('span');
+    new_color_key.classList.add('color-key');
+    new_color_key.style.backgroundColor = colors[cat];
+
+    const new_color_label = document.createElement('span');
+    new_color_label.classList.add('color-label');
+    new_color_label.innerText = cat;
+
+    new_cat_container.appendChild(new_color_key);
+    new_cat_container.appendChild(new_color_label);
+
+    leg.appendChild(new_cat_container);
+
+  })
+
+}
+
+make_color_legend();
+
 // carrega os dados
-fetch('dados.json').then(response => response.json()).then(data => {
+Promise.all([
 
-    //console.log(data.features);
+  fetch('dados_distritos.json').then(response => response.json()),
+  fetch('dados_municipio.json').then(response => response.json())
 
-    // dimensões do mapa
-    const w = 600;
-    const h = 600;
-    const pad = 30;
+]).then( files => {
 
-    // seleciona o elemento svg e define suas dimensões
-    const svg = d3.select('svg.map');
-    svg.attr('width', w);
-    svg.attr('height', h);
-    svg.attr('viewBox', `0 0 ${w} ${h}`);
+  // dimensões do mapa
+  //const w = 600;
+  //const h = 600;
 
-    // gera um objeto geojson correspondente à união dos distritos de SP,
-    // apenas para definir o center / scale / translation da projeção de forma automática com fitExtent
-    const sp = turf.combine(data);
+  // dados dos distritos propriamente ditos
+  const data = files[0];
+  console.log(data);
 
-    // define a projeção, usa o objeto geojson calculado acima para posicionar o mapa automaticamente dentro das dimensões desejadas
-    // (para evitar o pesadelo de usar .center().translate().scale() )
-    const proj = d3.geoTransverseMercator()
-      .fitExtent([[pad, pad], [w - pad, h - pad]], sp)
-    ;
+  // uma margem para o mapa
+  const pad = 10;
 
-    // define a função que vai pegar as coordenadas em lat/lon dos polígonos e vai transformá-las em coordenadas de desenho do SVG
-    const path = d3.geoPath().projection(proj);
+  // seleciona o elemento svg
+  const svg = d3.select('svg.map');
 
-    // cria os elementos path para cada distrito presente nos dados
-    const distritos = svg
-            .selectAll("path.distrito")
-            .data(data.features)
-            .join("path")
-            .classed('distrito', true)
-            .attr("d", path);
+  // capturamos as dimensoes do svg calculadas pelo browser
+  const wpx = svg.style('width');
+  const hpx = svg.style('height');
+  console.log(wpx,hpx);
 
-    // acrescenta um monitor de eventos do tipo "mouseover" nos elementos <path> que foram criados
-    distritos.on('mouseover', showInfo);
+  // só para remover os "px" das dimensões que capturamos
+  const w = +wpx.slice(0,-2);
+  const h = +hpx.slice(0,-2);
 
-    function showInfo(e) { 
+  // define os atributos do svg com base nessas dimensões
+  svg.attr('width', w);
+  svg.attr('height', h);
+  svg.attr('viewBox', `0 0 ${w} ${h}`);
+
+  // gera um objeto geojson correspondente à união dos distritos de SP,
+  // apenas para definir o center / scale / translation da projeção de forma automática com fitExtent
+  //const sp = turf.combine(data);
+
+  // o segundo arquivo é só um polígono do município de SP, para ser usado para centralizar e definir a escala ideal da projeção do mapa
+  const sp = files[1]; 
+
+  // define a projeção, usa o polígono acima para posicionar o mapa automaticamente dentro das dimensões desejadas
+  // (para evitar o pesadelo de usar .center().translate().scale() )
+  const proj = d3.geoMercator()
+    .fitExtent([[pad, pad], [w - pad, h - pad]], sp)
+  ;
+
+  // define a função que vai pegar as coordenadas lat/lon dos polígonos e vai transformá-las em coordenadas de desenho do SVG
+  const path = d3.geoPath().projection(proj);
+
+  // cria os elementos path para cada distrito presente nos dados
+  const distritos = svg
+          .selectAll("path.distrito")
+          .data(data.features)
+          .join("path")
+          .classed('distrito', true)
+          .attr("d", path)
+          .attr('fill', d => colors[d.properties.cat]);
+
+  // acrescenta um monitor de eventos do tipo "mouseover" nos elementos <path> que foram criados
+  distritos.on('mouseover', showInfo);
+
+  function showInfo(e, element_data) {
+
     // os monitores de evento (event listeners) sempre passam um argumento contendo detalhes do evento que foi disparado. 
     // por isso, sempre definimos um parämetro "e" na declaração de uma função que vai ser chamada por um monitor de eventos)
-        
-        // para entender o que esse argumento "e" carrega, vale dar um console.log nele e navegar um pouco pelo objeto no console...
-        //console.log(e);
+    // para entender o que esse argumento "e" carrega, vale dar um console.log nele e navegar um pouco pelo objeto no console...
+    //console.log(e);
 
-        // e.target é o elemento HTML em que foi disparado o evento. Na prática, é o elemento "path".
-        // quando esses elementos são criados com D3, os dados associados a esse elemento "path" ficam armazenados 
-        // em uma propriedade "__data__"
+    // como estamos usando o monitor de eventos do d3 (o ".on"), além de um parâmetro "e", é possível passar um segundo parâmetro que representa o dado que foi "amarrado" ao elemento que disparou o evento. Aqui, chamei de "element_data"
 
-        const element_data = e.target.__data__;
-        //console.log(element_data);
+    // a alternativa seria recuperar o dado a partir do atributo "e".
+    // e.target é o elemento HTML em que foi disparado o evento. Na prática, é o elemento "path".
+    // quando esses elementos são criados com D3, os dados associados a esse elemento "path" ficam armazenados 
+    // em uma propriedade "__data__"
 
-        const district_name = element_data.properties.NOME_DIST;
+    // então poderíamos buscar o "element_data" assim:
+    // const element_data = e.target.__data__;
+    // ou 
+    // const element_data = d3.select(e.target).datum();
 
-        // pronto, capturamos o nome do distrito. Agora vamos atualizar o conteúdo do elemento <span class="nome-distrito"> com esse nome
+    const district_name = element_data.properties.regiao;
 
-        // primeiro capturamos uma referência ao elemento
-        const name_container = document.querySelector('span.nome-distrito');
-        // agora atualizamos o seu conteúdo
-        name_container.innerText = district_name;
+    // pronto, capturamos o nome do distrito. Agora vamos atualizar o conteúdo do elemento <span class="nome-distrito"> com esse nome
 
-        // agora vamos aplicar uma classe de nome "highlight" ao elemento correspondente 
-        // (e remover essa classe de algum outro elemento que já a tenha)
-        // (com d3, essa operação é bem mais "sintética": "distritos" é uma referência à coleção de todos os elementos path, vamos passar de um por um e testar se o nome do distrito correspondente é igual ao nome do distrito sobre o qual o mouse está (esse nome está armazenado em "district_name"))
-        distritos.classed('highlight', d => d.properties.NOME_DIST == district_name);
+    // primeiro geramos uma referência ao elemento
+    const name_container = document.querySelector('span.nome-distrito');
+    // agora atualizamos o conteúdo do elemento
+    name_container.innerText = district_name;
 
-        // no css, definimos o que essa classe faz
+    // mesma coisa para o texto com dados
+    const texto_el = document.querySelector('span.dados-distrito');
+    const ndf = element_data.properties.ndf;
+    const ndp = element_data.properties.ndp;
+    const pct = element_data.properties.pct;
 
+    let texto;
+
+    if (ndf == 0) texto = 'não possui favelas.';
+    else {
+      texto = `possui <strong>${format(ndf)}</strong> favelas de um total de <strong>${format(ndp)}</strong> domicílios, o que representa um percentual de <strong>${format(pct)}%</strong>.`
     }
 
+    texto_el.innerHTML = texto;
+    
+
+    // agora vamos aplicar uma classe de nome "highlight" ao elemento correspondente 
+    // (e remover essa classe de algum outro elemento que já a tenha)
+    // (com d3, essa operação é bem mais "sintética": "distritos" é uma referência à coleção de todos os elementos path, vamos passar de um por um e testar se o nome do distrito correspondente é igual ao nome do distrito sobre o qual o mouse está (esse nome está armazenado em "district_name"))
+    distritos.classed('highlight', d => d.properties.regiao == district_name);
+
+    // no css, definimos o efeito que essa classe provoca!
+
+  }
 
 })
